b900d82c71970621d2dbadec94e502ab99397abf
petertaitl <peter.taitl@icloud.com>
2020-11-09 14:12:26 +0100

    Example Class angelegt und Besprechung der Befehle

---
+++ Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,18 @@
+﻿// Bibliotheken / Asembly Referenz / Namespaces
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Deklaration einer Klasse
+
+// public           -> access modifier (Zugriffsrechte) | keyword (vorrreservierte Schlüsselbegriffe)
+// class            -> keyword zur Deklaration einer Klasse
+// ExampleClass     -> identifier (Name der Klasse)
+// :MonoBehavior    -> Erstellen eine Unterjlasse von MonoBehaviour
+// {}               -> Scope/Wirkungsbereich (Was ist alles inb der Klasse enthalten)
+
+public class ExampleClass : MonoBehaviour
+{
+    
+}
