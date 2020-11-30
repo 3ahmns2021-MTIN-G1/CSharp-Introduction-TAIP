85bbd47e11bcefa7f3b8dae12808c2f398918a70
petertaitl <peter.taitl@icloud.com>
2020-11-30 12:01:09 +0100

    MasterScript added

---
+++ Assets/Scripts/Master.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Master : MonoBehaviour
+{
+    public float floatVarFirst;
+    public float floatVarSekond;
+    public Rigidbody rigidbodyVar;
+    public bool boolVar;
+    public BoxCollider boxColliderVar;
+    
+
+    void Update()
+    {
+        rigidbodyVar.mass = floatVarFirst + 20.0f;
+        rigidbodyVar.useGravity = boolVar;
+        rigidbodyVar.isKinematic = boolVar;
+        boxColliderVar.isTrigger = boolVar;
+        rigidbodyVar.angularDrag = floatVarFirst + 50.0f;
+        rigidbodyVar.drag = floatVarFirst - floatVarSekond;
+    }
+}
