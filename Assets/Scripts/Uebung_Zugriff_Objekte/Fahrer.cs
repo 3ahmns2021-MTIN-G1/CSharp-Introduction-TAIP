d2fef12665f2e827f97a55aedf309ae308058a34
petertaitl <peter.taitl@icloud.com>
2020-12-14 12:05:15 +0100

    Uebung_02_Zugriff auf Objekte und Variablen gemacht

---
+++ Assets/Scripts/Uebung_Zugriff_Objekte/Fahrer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fahrer : MonoBehaviour
+{
+    public string name;
+
+    public Auto car;
+
+    public void Update()
+    {
+        car.motor.isWorking = false;
+        car.links_hinten.name = "Honda";
+        car.links_hinten.isWorking = true;
+        car.rechts_vorne.name = "Toyota";
+        car.rechts_hinten.name = "Mitsubishi";
+        car.rechts_hinten.isWorking = true;
+        car.links_vorne.name = "Fiat";
+    }
+}
