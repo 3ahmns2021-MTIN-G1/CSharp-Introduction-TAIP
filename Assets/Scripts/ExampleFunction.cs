7c06781946fe766bb0b008eec88044551d27409e
petertaitl <peter.taitl@icloud.com>
2020-12-14 14:21:09 +0100

    Uebung_03_Deklaration und Aufruf einer Funktion gemacht

---
+++ Assets/Scripts/ExampleFunction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleFunction : MonoBehaviour
+{
+
+    public void FunktionInStart()
+    {
+        ZusatzFunktion();
+    }
+
+    public void FunktionInUpdate()
+    {
+
+    }
+
+    public void ZusatzFunktion()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FunktionInStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FunktionInUpdate();
+    }
+}
