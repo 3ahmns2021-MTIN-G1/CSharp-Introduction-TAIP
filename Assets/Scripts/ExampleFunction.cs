1fcd00041fffde64dc8971c2f32e74308985c3a7
petertaitl <peter.taitl@icloud.com>
2020-12-21 12:31:50 +0100

    ExampleFunction Erweiterung mit Paramenterliste

--- Assets/Scripts/ExampleFunction.cs
+++ Assets/Scripts/ExampleFunction.cs
@@ -20,10 +20,22 @@ public class ExampleFunction : MonoBehaviour
 
     }
 
+    public void RechenFunktion(float a, float b, float c)
+    {
+        float ergebnis = (a + b * c) / c;
+        print(ergebnis);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         FunktionInStart();
+
+        RechenFunktion(7, 7, 4);
+        RechenFunktion(12, 12, 5);
+        RechenFunktion(2, 2, 8);
+        RechenFunktion(4, 4, 342);
+        RechenFunktion(100, 100, 1);
     }
 
     // Update is called once per frame
