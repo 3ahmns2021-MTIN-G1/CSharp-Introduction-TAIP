80a950b2a4da80c3d7419f2abca4d36046b7a088
petertaitl <peter.taitl@icloud.com>
2020-12-28 21:57:28 +0100

    Aus modulo Klasse eine Rechner Klasse gemacht und erweitert

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    //......... Variablen Deklaration .........
+
+    public Rechner rechner;
+
+    public float floatVarOne;
+    public float floatVarTwo;
+    public string modulo;
+
+
+//.......... Funktionen Aufruf ..........
+
+    void Update()
+    {
+        modulo = rechner.ModuloFunktion(floatVarOne, floatVarTwo);   
+    }
+}
