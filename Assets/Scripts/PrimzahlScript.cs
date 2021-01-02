fcc5c7876e9754e2991f972c8fbfcee9df9aa875
petertaitl <peter.taitl@icloud.com>
2021-01-02 15:49:03 +0100

    Funktionen Deklaration in ein Operationen Script verlagert

--- Assets/Scripts/PrimzahlScript.cs
+++ Assets/Scripts/PrimzahlScript.cs
@@ -4,61 +4,16 @@ using UnityEngine.UI;
 
 public class PrimzahlScript : MonoBehaviour
 {
-    //.......... Variablen Deklaration ..........
-
     private int eingabe;
     private int eingabeTemp;
 
-    public int[] teilbarDurch;
-
     public InputField eingabeFeld;
     public Text ausgabeFeld;
-    
-
-    //.......... Funktionen Deklaration ..........
-
-    public int ModuloFunktion(int a, int b)
-    {
-        if (a > b)
-        {
-            int c = a / b;
-            int d = c * b;
-
-            return a - d;
-        }
-
-        else
-        {
-            return 0;
-        }
-    }
-
-    public bool IsPrimzahl(int a)
-    {
-        foreach (int i in teilbarDurch)
-        {
-           if (a == i)
-           {
-                return true;
-           }
-        }
-
-        for (int i = 2; i < 7; i++)
-        {
-           if (ModuloFunktion(a, i) == 0)
-           {
-                return false;
-           }
-        }
-
-        return true;
-    }
-
-    //.......... Funktionen Aufrufe ..........
-
+    public Operationen operationen; 
+     
     public void Update()
-    {
-        if (eingabeFeld.text == "")
+    {        
+        if (eingabeFeld.text == "" || eingabeFeld.text == " ")
         {
             ausgabeFeld.text = "Bitte eine Zahl eingeben";
             return;
@@ -67,14 +22,14 @@ public class PrimzahlScript : MonoBehaviour
         eingabe = int.Parse(eingabeFeld.text);
         eingabeTemp = eingabe;
 
-        if (IsPrimzahl(eingabe) == true)
+        if (operationen.IsPrimzahl(eingabe) == true)
         {
             ausgabeFeld.text = eingabe.ToString() + " ist eine Primzahl";
         }
 
         else
         {
-            while (IsPrimzahl(eingabeTemp) == false)
+            while (operationen.IsPrimzahl(eingabeTemp) == false)
             {
                 eingabeTemp++;
             }
