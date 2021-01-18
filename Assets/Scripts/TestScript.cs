04686478320130788a9553d397c8922e7666dae8
petertaitl <peter.taitl@icloud.com>
2021-01-18 14:28:22 +0100

    TestScript Erweiterung

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    //......... Variablen Deklaration .........
-
-    public Rechner rechner;
-
-    public float floatVarOne;
-    public float floatVarTwo;
-    public string modulo;
-
-
-//.......... Funktionen Aufruf ..........
+    public GameObject gameObjectVar;
 
     void Update()
     {
-        modulo = rechner.ModuloFunktion(floatVarOne, floatVarTwo);   
+        Mathf.Abs(1.1f);                                // Gibt den absoluten Wert eines Floats aus
+        Mathf.Approximately(1.1f, 2.2f);                // Vergeleicht 2 Werte ob sie ähnlich sind
+
+
+        gameObjectVar.CompareTag("Tag");                // Vergeleicht ein Gameobject mit einem Tag
+        GameObject.Find("Name");                        // Findet ein GameObject über seinen Namen
+
+        Vector3.Angle(new Vector3(), new Vector3());    // Gibt den Winkel zwischen zwei Vectoren aus
+
     }
 }
