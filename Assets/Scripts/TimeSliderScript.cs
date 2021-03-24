150022be336eed8f9bfa91299843f9d2ef830b76
Peter Taitl <peter.taitl@icloud.com>
2021-03-24 22:45:07 +0100

    MPJ Experimente

---
+++ Assets/Scripts/TimeSliderScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Formats.Alembic.Importer;
+
+public class TimeSliderScript : MonoBehaviour
+{
+    public Slider sd;
+    public GameObject hansi;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        //sp.timerange.from = sp.value;
+    }
+}
